d0f9276dc32b9914c3c066c315b488996929243c
leiming <mengchun19930211>
2018-10-24 15:59:56 +0800

    基础框架

---
+++ app/src/main/java/com/bsw/billbook/widget/timeselector/Utils/DateUtil.java
@@ -0,0 +1,52 @@
+package com.bsw.billbook.widget.timeselector.Utils;
+
+import android.text.TextUtils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by liuli on 2015/11/27.
+ */
+public class DateUtil {
+    /**
+     * 使用用户格式提取字符串日期
+     *
+     * @param strDate 日期字符串
+     * @param pattern 日期格式
+     * @return
+     */
+
+    public static Date parse(String strDate, String pattern) {
+        if (TextUtils.isEmpty(strDate)) {
+            return null;
+        }
+        try {
+            SimpleDateFormat df = new SimpleDateFormat(pattern, Locale.CHINA);
+            return df.parse(strDate);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 使用用户格式格式化日期
+     *
+     * @param date    日期
+     * @param pattern 日期格式
+     * @return
+     */
+
+    public static String format(Date date, String pattern) {
+        String returnValue = "";
+        if (date != null) {
+            SimpleDateFormat df = new SimpleDateFormat(pattern, Locale.CHINA);
+            returnValue = df.format(date);
+        }
+        return (returnValue);
+    }
+
+
+}
